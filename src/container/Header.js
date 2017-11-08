d9b0be12f7d2a103dab91c7d73511f3ed6ac9df9
Alexander Hesse <alexander.hesse@sandstorm-media.de>
2017-11-08 16:25:00 +0100

    FEATURE: better header

--- src/container/Header.js
+++ src/container/Header.js
@@ -2,18 +2,38 @@ import React from 'react'
 
 import MenuEntry from '../components/atoms/MenuEntry'
 
-const Header = () => {
-  return (
-    <header>
-      <nav>
-        <MenuEntry name="Basic"/>
-        <MenuEntry name="Stations"/>
-        <MenuEntry name="Skills"/>
-        <MenuEntry name="References"/>
-        <MenuEntry name="Hobbies"/>
-      </nav>
-    </header>
-  )
-}
+export default class Header extends React.PureComponent {
 
-export default Header
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      scrolled: false
+    }
+  }
+
+  render() {
+    return (
+      <header>
+        <nav>
+          <MenuEntry name="Basic"/>
+          <MenuEntry name="Stations"/>
+          <MenuEntry name="Skills"/>
+          <MenuEntry name="References"/>
+          <MenuEntry name="Hobbies"/>
+        </nav>
+      </header>
+    )
+  }
+
+  componentDidMount() {
+    const header = document.getElementsByTagName('header')[0];
+    window.addEventListener('scroll', function () {
+      if (document.documentElement.scrollTop > 200) {
+        header.setAttribute('class', 'scrolled')
+      } else {
+        header.removeAttribute('class')
+      }
+    })
+  }
+}
