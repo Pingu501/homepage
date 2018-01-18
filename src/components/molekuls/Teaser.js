8aefb80558356512d72755afb3cbbd22355182fd
Alexander Hesse <alexander.hesse@sandstorm-media.de>
2018-01-18 18:45:00 +0100

    better teaser mobile styling

--- src/components/molekuls/Teaser.js
+++ src/components/molekuls/Teaser.js
@@ -3,13 +3,24 @@ import PropTypes from 'prop-types'
 import { Parallax } from 'react-parallax'
 
 const Teaser = props => {
+
+  const backgroundImageStyle = {
+    backgroundImage: `url(${props.picture})`
+  }
+
   return (
     <div className="teaser__wrapper">
-      <Parallax bgImage={props.picture} strength={400} className="teaser">
+      <Parallax bgImage={props.picture} strength={400} className="teaser teaser__parallax">
         {props.text ? <div className="teaser__content">
           <p>{props.text}</p>
         </div> : ''}
       </Parallax>
+
+      <div className="teaser teaser__mobile" style={backgroundImageStyle}>
+        {props.text ? <div className="teaser__content">
+          <p>{props.text}</p>
+        </div> : ''}
+      </div>
     </div>
   )
 }
