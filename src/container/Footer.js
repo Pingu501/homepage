aa77249618f7d2e72c74b14a2f315aa11dddc714
Alexander Hesse <alexander.hesse@sandstorm-media.de>
2018-03-05 17:35:44 +0100

    moved legal to main page

--- src/container/Footer.js
+++ src/container/Footer.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import Legal from './Legal'
 
 export default class Footer extends React.PureComponent {
+  
   render () {
     return (
 
       <footer>
         <div className="footer__wrapper">
           &#169; Alexander Hesse, 2017 <br/>
-          <Link to="/legal">Legal Disclosure</Link>
+          <Legal/>
         </div>
       </footer>
     )
